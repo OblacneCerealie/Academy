8bb540878c16fad53c21651a18e5b3aff89a1556
OblacneCerealie <128903690+OblacneCerealie@users.noreply.github.com>
2023-05-14 00:14:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//var message;
+//message = Hello! I am an alert box!!; //mozem pisat poznamky
+
+//alert(message);
+//var user = "Surja" , age="29", message="Hello!", alive= true
+//var a ="moje" + "meno" ;
+//alert(a) ;
+//var a ="3" + "4" ;
+//alert(a) ;
+//var c= 5;
+//var d=6;
+//alert(c==d) ;
+//varxx= confirm-potvrdit, prompt
