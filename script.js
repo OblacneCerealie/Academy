d3aff0be1c47e422418ed704a460d8533defcb83
OblacneCerealie <bastymichalko@gmail.com>
2023-05-18 19:32:07 +0200

    Hodina 16.5
    Javascript nové učivo

--- script.js
+++ script.js
@@ -11,3 +11,18 @@
 //var d=6;
 //alert(c==d) ;
 //varxx= confirm-potvrdit, prompt
+//hey
+//function addTextNode(text) {
+//var newtext = document.createTextNode(text),
+    //p1 = document.getElementById("p1");
+    //p1.appendChild(newtext);
+//}
+function getNumber () {
+var a1 = document.getElementById('a')
+var b1 = document.getElementById('b')
+var c1 = document.getElementById('c')
+var a = Number (a1.value)
+var b = Number (b1.value)
+var c = a+b
+c1.innerHTML = c ;
+}
