65af83cb50ab602c4e0dd96669aebd6f20771a54
OblacneCerealie <bastymichalko@gmail.com>
2023-05-23 20:03:59 +0200

    Hodina 23.5.2023
    Slide changing JS.

--- script.js
+++ script.js
@@ -17,12 +17,24 @@
     //p1 = document.getElementById("p1");
     //p1.appendChild(newtext);
 //}
-function getNumber () {
-var a1 = document.getElementById('a')
-var b1 = document.getElementById('b')
-var c1 = document.getElementById('c')
-var a = Number (a1.value)
-var b = Number (b1.value)
-var c = a+b
-c1.innerHTML = c ;
+//function getNumber () {
+//var a1 = document.getElementById('a')
+//var b1 = document.getElementById('b')
+//var c1 = document.getElementById('c')
+//var a = Number (a1.value)
+//var b = Number (b1.value)
+//var c = a+b
+//c1.innerHTML = c ;
+//}
+document.addEventListener("DOMContentLoaded", function() {
+var slides = document.querySelectorAll("#slides li")
+var currentSlide = 0;
+var slideInterval = setInterval (nextSlide, 2000);
+
+function nextSlide (){
+    slides[currentSlide].className ="slide";
+    currentSlide = (currentSlide+1)%slides.length;
+    slides [currentSlide].className = "showing";
+
 }
+});
